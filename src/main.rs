6b38702e33788a8027b8f929eee694579b27bcf0
Marek Majerski <45997597+mmajerski@users.noreply.github.com>
2022-05-17 19:56:54 +0200

    telemetry (#2)
    * issues
    
    * CI issues
    
    * CI issues
    
    * CI issues
    
    * adding traces
    
    * finished telemetry
    
    * removing unnecessary reference

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 use rust_api::configuration::get_configuration;
 use rust_api::startup::run;
+use rust_api::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("rust_api".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
 
     let address = format!("localhost:{}", configuration.application_port);
 
