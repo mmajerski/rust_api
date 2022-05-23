f76f64084783d9b1ccaa04b8d36a8ae9024e9b43
Marek Majerski <45997597+mmajerski@users.noreply.github.com>
2022-05-23 20:14:52 +0200

    added subscriptions events(on publish) (#7)
    * added actions on message delivery event
    
    * updating sqlx-data.json

--- src/email_client.rs
+++ src/email_client.rs
@@ -28,7 +28,7 @@ impl EmailClient {
 
     pub async fn send_email(
         &self,
-        recipient: SubscriberEmail,
+        recipient: &SubscriberEmail,
         subject: &str,
         html_content: &str,
         text_content: &str,
@@ -136,7 +136,7 @@ mod tests {
 
         // act
         let outcome = email_client
-            .send_email(subscriber_email, &subject, &content, &content)
+            .send_email(&subscriber_email, &subject, &content, &content)
             .await;
 
         // assert
@@ -157,7 +157,7 @@ mod tests {
 
         // act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
@@ -182,7 +182,7 @@ mod tests {
 
         // act
         let _ = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
@@ -203,7 +203,7 @@ mod tests {
 
         // act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         // assert
